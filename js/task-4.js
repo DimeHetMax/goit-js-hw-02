9e9f39166a85d30d50e57ed7f70b8db1960ad121
Maksym Dimekhin <dimehinm@gmail.com>
2026-04-13 10:06:39 +0200

    homework

---
+++ js/task-4.js
@@ -0,0 +1,33 @@
+const getShippingCost = country => {
+  const countryObj = {
+    china: 100,
+    chile: 250,
+    australia: 170,
+    jamaica: 120,
+  };
+  let message;
+
+  switch (country) {
+    case 'China':
+      message = `"Shipping to ${country} will cost ${countryObj.china} credits",`;
+      break;
+    case 'Chile':
+      message = `"Shipping to ${country} will cost ${countryObj.chile} credits",`;
+      break;
+    case 'Australia':
+      message = `"Shipping to ${country} will cost ${countryObj.australia} credits",`;
+      break;
+    case 'Jamaica':
+      message = `"Shipping to ${country} will cost ${countryObj.jamaica} credits",`;
+      break;
+    default:
+      message = 'Sorry, there is no delivery to your country';
+  }
+  return message;
+};
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
